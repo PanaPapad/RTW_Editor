d13726b5188031d560411d3fe0e1f68cc9e4866d
Panagiotis <panagiotis---@live.com>
2025-10-30 01:24:35 +0200

    Expanded Factions const to include all factions

--- src/lib/consts.js
+++ src/lib/consts.js
@@ -9,13 +9,103 @@ export const FACTIONS = [
     culture: "roman",
   },
   {
+    name: "Scipii",
+    id: "romans_scipii",
+    culture: "roman",
+  },
+  {
+    name: "Brutii",
+    id: "romans_brutii",
+    culture: "roman",
+  },
+  {
+    name: "Julii",
+    id: "romans_julii",
+    culture: "roman",
+  },
+  {
     id: "seleucid",
     name: "Seleucids",
+    culture: "greek",
+  },
+  {
+    id: "greek_cities",
+    name: "Greek Cities",
+    culture: "greek",
+  },
+  {
+    id: "macedon",
+    name: "Macedon",
+    culture: "greek",
+  },
+  {
+    id: "thrace",
+    name: "Thrace",
     culture: "greek",
   },
   {
     id: "egypt",
     name: "Egypt",
     culture: "egyptian",
+  },
+  {
+    id: "carthage",
+    name: "Carthage",
+    culture: "carthaginian",
+  },
+  {
+    id: "numidia",
+    name: "Numidia",
+    culture: "carthaginian",
+  },
+  {
+    id: "pontus",
+    name: "Pontus",
+    culture: "eastern",
+  },
+  {
+    id: "armenia",
+    name: "Armenia",
+    culture: "eastern",
+  },
+  {
+    id: "parthia",
+    name: "Parthia",
+    culture: "eastern",
+  },
+  {
+    id: "gauls",
+    name: "Gaul",
+    culture: "barbarian",
+  },
+  {
+    id: "britons",
+    name: "Brittania",
+    culture: "barbarian",
+  },
+  {
+    id: "germans",
+    name: "Germania",
+    culture: "barbarian",
+  },
+  {
+    id: "dacia",
+    name: "Dacia",
+    culture: "barbarian",
+  },
+  {
+    id: "scythia",
+    name: "Scythia",
+    culture: "barbarian",
+  },
+  {
+    id: "spain",
+    name: "Spain",
+    culture: "barbarian",
+  },
+  {
+    id: "slave",
+    name: "Rebels",
+    culture: "barbarian",
   },
 ];
